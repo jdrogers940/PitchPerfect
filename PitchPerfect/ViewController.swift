cc43eca3bd4c13793f7856da5a0f8dd7c3c0549b
jdrogers940 <Josh@Joshuas-MacBook-Pro.local>
2016-08-15 16:43:00 -0400

    View Controller and Multiple Views
    Through the above section of the Udacity course.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.enabled = true
+        recordButton.enabled = false
+    }
 
+    @IBAction func stopAudio(sender: AnyObject) {
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.enabled = false
+        recordButton.enabled = true
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingButton.enabled = false
+    }
 }
 
